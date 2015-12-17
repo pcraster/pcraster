07276eca70345a28e6bbeb824ef3c6c1a118d714
Kor de Jong <k.dejong1@uu.nl>
2015-12-17 10:32:25 +0100

    api

--- source/api/fieldapi_readwritedatatest.cc
+++ source/api/fieldapi_readwritedatatest.cc
@@ -1,82 +1,15 @@
-#ifndef INCLUDED_STDDEFX
-#include "stddefx.h"
-#define INCLUDED_STDDEFX
-#endif
-
-#ifndef INCLUDED_FIELDAPI_READWRITEDATATEST
-#include "fieldapi_readwritedatatest.h"
-#define INCLUDED_FIELDAPI_READWRITEDATATEST
-#endif
-
-// Library headers.
-#ifndef INCLUDED_BOOST_SHARED_PTR
-#include <boost/shared_ptr.hpp>
-#define INCLUDED_BOOST_SHARED_PTR
-#endif
-
-#ifndef INCLUDED_BOOST_TEST_TEST_TOOLS
-#include <boost/test/test_tools.hpp>
-#define INCLUDED_BOOST_TEST_TEST_TOOLS
-#endif
-
-#ifndef INCLUDED_BOOST_TEST_UNIT_TEST_SUITE
-#include <boost/test/unit_test_suite.hpp>
-#define INCLUDED_BOOST_TEST_UNIT_TEST_SUITE
-#endif
-
-// PCRaster library headers.
-#ifndef INCLUDED_PCRTYPES
+#define BOOST_TEST_MODULE pcraster fieldapi read_write_data
+#include <boost/test/unit_test.hpp>
 #include "pcrtypes.h"
-#define INCLUDED_PCRTYPES
-#endif
-
-// Module headers.
-#ifndef INCLUDED_FIELDAPI_READWRITEDATA
 #include "fieldapi_readwritedata.h"
-#define INCLUDED_FIELDAPI_READWRITEDATA
-#endif
-#ifndef INCLUDED_FIELDAPI_TESTFIELD
 #include "fieldapi_testfield.h"
-#define INCLUDED_FIELDAPI_TESTFIELD
-#endif
 
-
-/*!
-  \file
-  This file contains the implementation of the ReadWriteDataTest class.
-*/
-
-
-
-//------------------------------------------------------------------------------
-// DEFINITION OF STATIC READWRITEDATA MEMBERS
-//------------------------------------------------------------------------------
-
-//! suite
-boost::unit_test::test_suite*fieldapi::ReadWriteDataTest::suite()
-{
-  boost::unit_test::test_suite* suite = BOOST_TEST_SUITE(__FILE__);
-  boost::shared_ptr<ReadWriteDataTest> instance(new ReadWriteDataTest());
-
-  suite->add(BOOST_CLASS_TEST_CASE(&ReadWriteDataTest::testAll, instance));
-
-  return suite;
-}
-
-
-
-//------------------------------------------------------------------------------
-// DEFINITION OF READWRITEDATA MEMBERS
-//------------------------------------------------------------------------------
-
-//! ctor
-fieldapi::ReadWriteDataTest::ReadWriteDataTest()
-{
-}
 
 template<class UseAsT,class StoredAsT>
-void fieldapi::ReadWriteDataTest::testType()
+void testType()
 {
+  using namespace fieldapi;
+
    StoredAsT  d1[ ] = { 0, 1, 2 , 3, 40, 5 };
    pcr::setMV(d1[4]);
    TestField<StoredAsT,2,3> d2(d1);
@@ -129,8 +62,8 @@ void fieldapi::ReadWriteDataTest::testType()
         BOOST_CHECK(!t.get(v,r,c));
 }
 
-//! testall
-void fieldapi::ReadWriteDataTest::testAll()
+
+BOOST_AUTO_TEST_CASE(all)
 {
  testType<UINT1,UINT1>();
  testType<INT4, UINT1>();

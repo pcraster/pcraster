4c0603813d1ad8254da21caed69fa035dc9149b5
Oliver Schmitz <o.schmitz@uu.nl>
2019-01-08 14:36:14 +0100

    WIP travis

--- environment/travis/linux/before_install.sh
+++ environment/travis/linux/before_install.sh
@@ -7,8 +7,8 @@ sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable -y
 sudo apt-get update -qq
 
 # Update CMake.
-wget --no-check-certificate http://www.cmake.org/files/v3.12/cmake-3.12.1-Linux-x86_64.tar.gz
+# wget --no-check-certificate http://www.cmake.org/files/v3.12/cmake-3.12.1-Linux-x86_64.tar.gz
 
 # Travis Qwt 6.0 does not cooperate with Qt5
 # We build and install those ourselves using Peacock.
-git clone --recursive https://github.com/geoneric/peacock.git
+# git clone --recursive https://github.com/geoneric/peacock.git
--- environment/travis/linux/install.sh
+++ environment/travis/linux/install.sh
@@ -10,7 +10,9 @@ cwdir=${PWD}
 sudo apt-get install qt56base
 export PATH=/opt/qt56/bin:$PATH
 
-sudo apt-get install gdal-bin libgdal-dev libxerces-c-dev libxml2-dev libxslt1-dev libboost-all-dev libncurses5-dev libpython-dev libxml2 libxml2-utils mesa-common-dev
+sudo apt-get install libqwt-qt5-dev
+
+sudo apt-get install gdal-bin libgdal-dev libxerces-c-dev libxml2-dev libxslt1-dev libboost-all-dev libncurses5-dev libpython-dev libxml2 libxml2-utils mesa-common-dev cmake
 
 
 
@@ -19,23 +21,23 @@ pip install --user --upgrade pip
 pip install --user --upgrade numpy
 pip install --user --upgrade docopt
 pip install --user --upgrade sphinx
-pip install --user --upgrade pillow
+# pip install --user --upgrade pillow
 pip install --user --upgrade psutil
 
 
-mkdir $TRAVIS_BUILD_DIR/local
+# mkdir $TRAVIS_BUILD_DIR/local
 
-cd $TRAVIS_BUILD_DIR/local
-tar zxf ../cmake-3.12.1-Linux-x86_64.tar.gz
-export PATH=$TRAVIS_BUILD_DIR/local/cmake-3.12.1-Linux-x86_64/bin:$PATH
+# cd $TRAVIS_BUILD_DIR/local
+# tar zxf ../cmake-3.12.1-Linux-x86_64.tar.gz
+# export PATH=$TRAVIS_BUILD_DIR/local/cmake-3.12.1-Linux-x86_64/bin:$PATH
 
-cd $TRAVIS_BUILD_DIR/peacock
-mkdir build
-cd build
-echo $CC
-echo $CXX
-cmake -G"${TRAVIS_CMAKE_GENERATOR}" -Dpeacock_prefix=$TRAVIS_BUILD_DIR/local -Dbuild_qwt=true -Dqwt_version=6.1.2 ..
-cmake --build . --target all -- -j2
+# cd $TRAVIS_BUILD_DIR/peacock
+# mkdir build
+# cd build
+# echo $CC
+# echo $CXX
+# cmake -G"${TRAVIS_CMAKE_GENERATOR}" -Dpeacock_prefix=$TRAVIS_BUILD_DIR/local -Dbuild_qwt=true -Dqwt_version=6.1.2 ..
+# cmake --build . --target all -- -j2
 
 
 # Return to initial directory

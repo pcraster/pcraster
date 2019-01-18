1d525f732d50788b87816a6f011b912589214f71
Oliver Schmitz <o.schmitz@uu.nl>
2019-01-18 10:27:53 +0100

    Cleaned up travis scripts

--- environment/travis/linux/before_install.sh
+++ environment/travis/linux/before_install.sh
@@ -5,10 +5,3 @@ set -v
 sudo add-apt-repository ppa:beineri/opt-qt-5.12.0-xenial -y
 sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable -y
 sudo apt-get update -qq
-
-# Update CMake.
-# wget --no-check-certificate http://www.cmake.org/files/v3.12/cmake-3.12.1-Linux-x86_64.tar.gz
-
-# Travis Qwt 6.0 does not cooperate with Qt5
-# We build and install those ourselves using Peacock.
-# git clone --recursive https://github.com/geoneric/peacock.git
--- environment/travis/linux/install.sh
+++ environment/travis/linux/install.sh
@@ -14,31 +14,12 @@ sudo apt-get install libqwt-qt5-dev
 
 sudo apt-get install gdal-bin libgdal-dev libxerces-c-dev libxml2-dev libxslt1-dev libboost-all-dev libncurses5-dev libpython-dev libxml2 libxml2-utils mesa-common-dev libglu1-mesa-dev libgl1-mesa-glx cmake
 
-
-
 cd $TRAVIS_BUILD_DIR
 pip install --user --upgrade pip
 pip install --user --upgrade numpy
 pip install --user --upgrade docopt
 pip install --user --upgrade sphinx
-# pip install --user --upgrade pillow
 pip install --user --upgrade psutil
-
-
-# mkdir $TRAVIS_BUILD_DIR/local
-
-# cd $TRAVIS_BUILD_DIR/local
-# tar zxf ../cmake-3.12.1-Linux-x86_64.tar.gz
-# export PATH=$TRAVIS_BUILD_DIR/local/cmake-3.12.1-Linux-x86_64/bin:$PATH
-
-# cd $TRAVIS_BUILD_DIR/peacock
-# mkdir build
-# cd build
-# echo $CC
-# echo $CXX
-# cmake -G"${TRAVIS_CMAKE_GENERATOR}" -Dpeacock_prefix=$TRAVIS_BUILD_DIR/local -Dbuild_qwt=true -Dqwt_version=6.1.2 ..
-# cmake --build . --target all -- -j2
-
 
 # Return to initial directory
 cd ${cwdir}

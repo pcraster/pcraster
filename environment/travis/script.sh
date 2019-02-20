60ae5e0e4ac27ab118beae5631e15c1a127aecba
Oliver Schmitz <o.schmitz@uu.nl>
2019-02-20 09:19:43 +0100

    Wip travis

--- environment/travis/script.sh
+++ environment/travis/script.sh
@@ -5,8 +5,8 @@ set -v
 
 echo $CC
 echo $CXX
-python --version
-python -c "import numpy as n; print(n.__version__); print(n.get_include());"
+python$TRAVIS_PYTHON_VERSION --version
+python$TRAVIS_PYTHON_VERSION -c "import numpy as n; print(n.__version__); print(n.get_include());"
 
 cd $TRAVIS_BUILD_DIR/build
 cmake --build . --config ${TRAVIS_BUILD_TYPE} --target all -- -j2

7e04e45160ef04ea93005b449614bf23aa23acb9
Oliver Schmitz <o.schmitz@uu.nl>
2019-06-07 12:00:18 +0200

    Refactored default flags for release build

--- environment/cmake/gcc.cmake
+++ environment/cmake/gcc.cmake
@@ -18,7 +18,7 @@ set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,--no-undefined")
 
 
 set(PCR_C_FLAGS
-  "-march=native -Wall -Wextra -Wno-pedantic -Wwrite-strings -Werror=strict-aliasing -fno-strict-overflow -Wno-implicit-fallthrough -Wno-cpp -Wno-cast-qual -Wno-maybe-uninitialized -Wno-unused-parameter -Wno-empty-body -Wno-unused-but-set-variable -Wno-unused-function -Wno-switch -Wno-empty-body -Wno-sign-compare -Wno-float-equal -Wno-implicit-function-declaration -Wno-cast-function-type -Wno-unused-variable"
+  "-Wall -Wextra -Wno-pedantic -Wwrite-strings -Werror=strict-aliasing -fno-strict-overflow -Wno-implicit-fallthrough -Wno-cpp -Wno-cast-qual -Wno-maybe-uninitialized -Wno-unused-parameter -Wno-empty-body -Wno-unused-but-set-variable -Wno-unused-function -Wno-switch -Wno-empty-body -Wno-sign-compare -Wno-float-equal -Wno-implicit-function-declaration -Wno-cast-function-type -Wno-unused-variable"
 )
 
 set(PCR_C_FLAGS_GCC7
@@ -32,7 +32,7 @@ set(PCR_C_FLAGS_GCC8
 
 
 set(PCR_CXX_FLAGS
-  "-march=native -Wall -Wextra -Wno-pedantic -Wwrite-strings -Werror=strict-aliasing -ftemplate-backtrace-limit=0 -fno-strict-overflow -Wno-unused-variable -Wno-unused-result -Wno-implicit-fallthrough  -Wno-cpp -Wno-parentheses -Wno-cast-qual -Wno-maybe-uninitialized -Wno-unused-parameter -Wno-empty-body -Wno-unused-but-set-variable -Wno-ignored-qualifiers -Wno-switch -Wno-write-strings -Wno-unused-local-typedefs -Wno-delete-non-virtual-dtor -Wno-unused-label -Wno-sign-compare -Wno-float-equal -Wno-misleading-indentation -Wno-deprecated-declarations -Wduplicated-cond  -Wno-null-dereference  -Wno-useless-cast -Wno-old-style-cast -Wno-double-promotion -Wformat=2 -Wno-shadow -Wno-format-nonliteral -Wno-suggest-final-types -Wno-suggest-final-methods -Wno-cast-function-type"
+  "-Wall -Wextra -Wno-pedantic -Wwrite-strings -Werror=strict-aliasing -ftemplate-backtrace-limit=0 -fno-strict-overflow -Wno-unused-variable -Wno-unused-result -Wno-implicit-fallthrough  -Wno-cpp -Wno-parentheses -Wno-cast-qual -Wno-maybe-uninitialized -Wno-unused-parameter -Wno-empty-body -Wno-unused-but-set-variable -Wno-ignored-qualifiers -Wno-switch -Wno-write-strings -Wno-unused-local-typedefs -Wno-delete-non-virtual-dtor -Wno-unused-label -Wno-sign-compare -Wno-float-equal -Wno-misleading-indentation -Wno-deprecated-declarations -Wduplicated-cond  -Wno-null-dereference  -Wno-useless-cast -Wno-old-style-cast -Wno-double-promotion -Wformat=2 -Wno-shadow -Wno-format-nonliteral -Wno-suggest-final-types -Wno-suggest-final-methods -Wno-cast-function-type"
 )
 
 #  -Wno-catch-value

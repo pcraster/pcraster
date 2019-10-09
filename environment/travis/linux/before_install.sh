7af57ee26f016340ac0e6c6917dfe12c833c5716
Oliver Schmitz <o.schmitz@uu.nl>
2019-10-09 15:05:47 +0200

    Fix for travis

--- environment/travis/linux/before_install.sh
+++ environment/travis/linux/before_install.sh
@@ -2,7 +2,7 @@
 set -e
 set -v
 
-sudo add-apt-repository ppa:beineri/opt-qt-5.12.0-xenial -y
+sudo add-apt-repository ppa:beineri/opt-qt-5.12.0-bionic -y
 # sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable -y
 sudo apt-get update -qq
 

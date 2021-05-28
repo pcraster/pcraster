4e4dac12d50911a9998ffc2887842ac77c0153f3
Oliver Schmitz <o.schmitz@uu.nl>
2021-05-28 12:08:12 +0200

    Bumped pybind11 and rasterformat

--- environment/cmake/PCRasterConfiguration.cmake
+++ environment/cmake/PCRasterConfiguration.cmake
@@ -258,7 +258,7 @@ if(NOT PYBIND11_SYSTEM_INCLUDE)
     FetchContent_Declare(
         pybind11
         GIT_REPOSITORY https://github.com/pybind/pybind11
-        GIT_TAG        v2.4.3
+        GIT_TAG        v2.6.2
     )
 
     FetchContent_GetProperties(pybind11)

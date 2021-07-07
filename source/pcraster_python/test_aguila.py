c30065d5be162e85a8456f88e4efc49a69929330
Oliver Schmitz <o.schmitz@uu.nl>
2021-07-07 08:27:01 +0200

    Fixed aguila for dirs without write permission

--- source/pcraster_python/test_aguila.py
+++ source/pcraster_python/test_aguila.py
@@ -1,4 +1,6 @@
+import os
 import pathlib
+import sys
 import unittest
 
 import pcraster
@@ -42,5 +44,27 @@ class TestAguila(testcase.TestCase):
 
 
         with self.assertRaises(Exception) as context_manager:
-            pcraster.aguila('--timesteps=[1, 10] q', pcraster_unit_test=False)
+            pcraster.aguila('--timesteps=[1, 10] q', pcraster_unit_test=True)
         self.assertEqual(str(context_manager.exception), f'aguila --timesteps=[1, 10] q')
+
+
+    def test_02(self):
+        """ aguila executed in directories without write permission """
+
+        if sys.platform.startswith('linux'):
+          test_path = pathlib.Path('/usr/')
+          expected = 'aguila /tmp/tmp'
+        elif sys.platform.startswith('win32'):
+          test_path = pathlib.Path('C:', 'Windows')
+          tmp = os.environ['TEMP']
+          expected = f'aguila.exe {tmp}'
+        elif sys.platform.startswith('darwin'):
+          test_path = pathlib.Path('/System/')
+          tmp = os.environ['TMPDIR']
+          expected = f'aguila {tmp}'
+
+        os.chdir(test_path)
+        pcraster.setclone(5, 4, 3, 2, 1)
+
+        with self.assertRaisesRegex(Exception, expected):
+            pcraster.aguila(pcraster.uniform(1), pcraster_unit_test=True)

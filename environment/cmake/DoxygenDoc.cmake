7fa49cac50de40493b0930860183396c87871561
Oliver Schmitz <o.schmitz@uu.nl>
2021-12-08 08:55:48 +0100

    Fixed Windows flags; documentation build

--- environment/cmake/DoxygenDoc.cmake
+++ environment/cmake/DoxygenDoc.cmake
@@ -1,16 +1,10 @@
-# Only generate the TODO page in Debug configurations. Note that this only
-# works for single-configuration generators like make. The idea is not to
+# Only generate the TODO page in Debug configurations. The idea is not to
 # install a TODO list page in the API docs when releasing a project.
-set(DEVBASE_DOXYGEN_GENERATE_TODOLIST "NO")
-if(${CMAKE_BUILD_TYPE} STREQUAL "Debug")
-    set(DEVBASE_DOXYGEN_GENERATE_TODOLIST "YES")
-endif()
 
-# DEVBASE_DOXYGEN_INPUT is a list of pathnames. Turn it into a string with
-# pathnames separated by spaces.
-string(REPLACE ";" " " DEVBASE_DOXYGEN_INPUT "${DEVBASE_DOXYGEN_INPUT}")
-
-set(DOXYGEN_TEMPLATE "
+file (GENERATE
+    OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile_$<CONFIG>
+    CONTENT "
+    PROJECT_NAME            = PCRaster
     ALWAYS_DETAILED_SEC     = YES
     BUILTIN_STL_SUPPORT     = YES
     CLASS_DIAGRAMS          = YES
@@ -20,12 +14,13 @@ set(DOXYGEN_TEMPLATE "
     EXTRACT_ALL             = YES
     EXTRACT_PRIVATE         = NO
     EXTRACT_STATIC          = NO
-    FILE_PATTERNS           = *.h *.hpp *.hxx *.c *.cc *.cpp *.cxx *.dox *.md ${DEVBASE_DOXYGEN_EXTERNAL_SOURCES_FILE_PATTERNS}
+    FILE_PATTERNS           = *.h *.hpp *.hxx *.c *.cc *.cpp *.cxx *.dox *.md
     FULL_PATH_NAMES         = YES
     GENERATE_LATEX          = NO
-    GENERATE_TODOLIST       = ${DEVBASE_DOXYGEN_GENERATE_TODOLIST}
-    INPUT                   = ${DEVBASE_DOXYGEN_INPUT}
+    GENERATE_TODOLIST       = $<IF:$<CONFIG:Release>,NO,YES>
+    INPUT                   = ${CMAKE_CURRENT_SOURCE_DIR}
     HAVE_DOT                = YES
+    DOT_GRAPH_MAX_NODES     = 75
     INCLUDE_GRAPH           = YES
     INHERIT_DOCS            = YES
     INLINE_INFO             = YES
@@ -48,21 +43,18 @@ set(DOXYGEN_TEMPLATE "
     WARN_IF_UNDOCUMENTED    = NO  # Because EXTRACT_ALL is turned on.
     WARNINGS                = YES
     WARN_NO_PARAMDOC        = YES
-")
-
-configure_file(
-    ${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile.in
-    ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
+    OUTPUT_DIRECTORY        = ${PROJECT_BINARY_DIR}/api_docs/$<CONFIG>
+    "
 )
 
 add_custom_command(
-    OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/html/index.html
-    COMMAND ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
-    DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
+    OUTPUT ${PROJECT_BINARY_DIR}/api_docs/$<CONFIG>/html/index.html
+    COMMAND ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile_$<CONFIG>
+    DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile_$<CONFIG>
 )
 
 # This target should be built by default, because otherwise the results won't
 # be available when the user wants to install the generated documentation
 add_custom_target(cpp_doc ALL
-    DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/html/index.html
+    DEPENDS ${PROJECT_BINARY_DIR}/api_docs/$<CONFIG>/html/index.html
 )
--- environment/cmake/msvs2017.cmake
+++ environment/cmake/msvs2017.cmake
@@ -1,4 +1,3 @@
-
 set(CMAKE_C_FLAGS
     "${CMAKE_C_FLAGS} /DWIN32 /D_WINDOWS /W3 /wd4267 /wd4251"
 )
@@ -9,20 +8,20 @@ set(CMAKE_CXX_FLAGS
 
 
 set(CMAKE_C_FLAGS_RELEASE
-    "${CMAKE_C_FLAGS_DEBUG} /O2"
+    "${CMAKE_C_FLAGS_RELEASE} /O2"
 )
 
 set(CMAKE_CXX_FLAGS_RELEASE
-    "${CMAKE_CXX_FLAGS_DEBUG} /O2"
+    "${CMAKE_CXX_FLAGS_RELEASE} /O2"
 )
 
 
 set(CMAKE_C_FLAGS_RELWITHDEBINFO
-    "${CMAKE_C_FLAGS_DEBUG} /O2"
+    "${CMAKE_C_FLAGS_RELWITHDEBINFO} /O2"
 )
 
 set(CMAKE_CXX_FLAGS_RELWITHDEBINFO
-    "${CMAKE_CXX_FLAGS_DEBUG} /O2"
+    "${CMAKE_CXX_FLAGS_RELWITHDEBINFO} /O2"
 )
 
 
@@ -58,14 +57,11 @@ add_compile_definitions(
 #         -D_ALLOW_KEYWORD_MACROS
 #     )
 #
-#     set(CMAKE_C_FLAGS
-#         "${CMAKE_C_FLAGS} /wd4267"
-#     )
 #
 #     # add /w3
 #     # disable these warnings?
 #     set(CMAKE_CXX_FLAGS
-#         "${CMAKE_CXX_FLAGS} /std:c++14 /wd4267 /wd4251 /wd4244 /wd4396 /wd4305"
+#         "${CMAKE_CXX_FLAGS} /std:c++14 /wd4244 /wd4396 /wd4305"
 #     )
 #
 # endif()

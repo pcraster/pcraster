8e0401a62abda67608dd34a96d626678063e57cf
Oliver Schmitz <o.schmitz@uu.nl>
2022-03-18 08:31:21 +0100

    WIP CI test matrix

---
+++ environment/configuration/receipe/build.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -e
+
+
+if [ $(uname) == Linux ]; then
+  # Kindly guide to conda's OpenGL...
+  PLATFORM_OPTIONS="-D OPENGL_opengl_LIBRARY:PATH=${BUILD_PREFIX}/${HOST}/sysroot/usr/lib64/libGL.so \
+  -D OPENGL_gl_LIBRARY:PATH=${BUILD_PREFIX}/${HOST}/sysroot/usr/lib64/libGL.so \
+  -D OPENGL_glu_LIBRARY:PATH=${PREFIX}/lib/libGLU.so"
+else
+  PLATFORM_OPTIONS="-D GDAL_INCLUDE_DIR=${PREFIX}/include"
+  export TMPDIR=/tmp
+fi
+
+
+# We need to create an out of source build
+cd $SRC_DIR
+
+mkdir -p build && cd build
+
+
+# master branch
+cmake .. -G"Ninja" -DCMAKE_BUILD_TYPE=Release \
+  -D CMAKE_INSTALL_PREFIX="${PREFIX}" \
+  -D PCRASTER_PYTHON_INSTALL_DIR=${SP_DIR} \
+  -D PCRASTER_WITH_FLAGS_NATIVE=OFF \
+  -D PCRASTER_BUILD_TEST=ON \
+  -D PCRASTER_BUILD_MODFLOW_EXECUTABLE=OFF \
+  $PLATFORM_OPTIONS
+
+cmake --build . --target all
+
+export LOGNAME=pcrtester
+
+ctest --output-on-failure
+
+cmake --build . --target install
---
+++ environment/configuration/receipe/run_test.py
@@ -0,0 +1,22 @@
+import pcraster as pcr
+import pcraster.framework as pcrfw
+import pcraster.multicore as pcrmc
+import pcraster.mldd as pcrmldd
+
+pcr.setclone(5, 4, 3, 2, 1)
+
+raster = pcr.uniform(1)
+
+pcrmc.set_nr_worker_threads(2)
+
+class UserModel(pcrfw.StaticModel):
+    def __init__(self):
+        pcrfw.StaticModel.__init__(self)
+
+    def initial(self):
+        print(f"PCRaster version: {pcr.__version__}")
+
+myModel = UserModel()
+staticModel = pcrfw.StaticFramework(myModel)
+staticModel.run()
+

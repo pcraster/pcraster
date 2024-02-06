bf9ea5da7995187addc3e02ef4fcd567ed57e08d
Oliver Schmitz <o.schmitz@uu.nl>
2024-02-06 18:12:48 +0100

    WIP CI

--- environment/configuration/receipe/run_test.py
+++ environment/configuration/receipe/run_test.py
@@ -2,6 +2,8 @@ import pcraster as pcr
 import pcraster.framework as pcrfw
 import pcraster.multicore as pcrmc
 import pcraster.mldd as pcrmldd
+import pcraster.collection as pcrcol
+import pcraster.moc as pcrmoc
 
 pcr.setclone(5, 4, 3, 2, 1)
 

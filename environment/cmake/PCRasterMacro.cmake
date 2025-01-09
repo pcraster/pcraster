8a644690f803990cfc24685b278c13a396cf579e
Oliver Schmitz <o.schmitz@uu.nl>
2025-01-09 19:04:19 +0100

    Removed find_python_module

--- environment/cmake/PCRasterMacro.cmake
+++ environment/cmake/PCRasterMacro.cmake
@@ -20,25 +20,3 @@ macro(add_file_dependency TARGET)
     # SET_SOURCE_FILES_PROPERTIES(${TARGET}
     #   PROPERTIES OBJECT_DEPENDS "${${TARGET}_deps}")
 endmacro(add_file_dependency)
-
-# Based on http://www.cmake.org/pipermail/cmake/2011-January/041666.html
-function(find_python_module module)
-    string(TOUPPER ${module} module_upper)
-    if(NOT PYTHON_MODULE_${module_upper})
-        if(ARGC GREATER 1 AND ARGV1 STREQUAL "REQUIRED")
-            set(PYTHON_MODULE_${module_upper}_FIND_REQUIRED TRUE)
-        endif()
-        # A module's location is usually a directory, but for binary modules
-        # it's a .so file.
-        execute_process(COMMAND "${Python_EXECUTABLE}" "-c"
-            "import re, ${module}; print(re.compile('/__init__.py.*').sub('',${module}.__file__))"
-            RESULT_VARIABLE _${module}_status
-            OUTPUT_VARIABLE _${module}_location
-            ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
-        if(NOT _${module}_status)
-            set(PYTHON_MODULE_${module_upper} ${_${module}_location} CACHE STRING
-                "Location of Python module ${module}")
-        endif(NOT _${module}_status)
-    endif(NOT PYTHON_MODULE_${module_upper})
-    find_package_handle_standard_args(PYTHON_MODULE_${module_upper} DEFAULT_MSG PYTHON_MODULE_${module_upper})
-endfunction(find_python_module)

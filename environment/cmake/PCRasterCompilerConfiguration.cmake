5226f776e530f54ec9cf3527071ddffe85ec1eeb
Oliver Schmitz <o.schmitz@uu.nl>
2025-02-24 19:38:56 +0100

    Updated linker flags

--- environment/cmake/PCRasterCompilerConfiguration.cmake
+++ environment/cmake/PCRasterCompilerConfiguration.cmake
@@ -88,9 +88,7 @@ add_compile_definitions(
 # https://wiki.ubuntu.com/ToolChain/CompilerFlags?action=show&redirect=CompilerFlags
 # https://wiki.debian.org/Hardening
 add_link_options(
-    "$<$<OR:$<CXX_COMPILER_ID:GNU>>:-Wl,--no-undefined;-Wl,--as-needed;-Wl,--sort-common;-Wl,--gc-sections;-Wl,-O2;-Wl,-z,now;-Wl,-z,relro;-Wl,-z,defs;-Wl,--warn-common;-Wl,--hash-style=gnu;-Wl,--no-copy-dt-needed-entries>"
-    "$<$<AND:$<PLATFORM_ID:Linux>,$<CXX_COMPILER_ID:Clang>>:-Wl,--as-needed;-Wl,--sort-common;-Wl,--gc-sections;-Wl,-O2;-Wl,-z,now;-Wl,-z,relro;-Wl,--warn-common;-Wl,--hash-style=gnu;-Wl,--no-copy-dt-needed-entries>"
-
+    "$<$<OR:$<CXX_COMPILER_ID:GNU>,$<CXX_COMPILER_ID:Clang>>:-Wl,-O2;-Wl,--sort-common;-Wl,--as-needed;-Wl,-z,relro;-Wl,-z,now;-Wl,--disable-new-dtags;-Wl,--gc-sections;-Wl,--allow-shlib-undefined;-Wl,--warn-common;-Wl,--no-copy-dt-needed-entries>"
 )
 
 if(PCRASTER_WITH_FLAGS_IPO)

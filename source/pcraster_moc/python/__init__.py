239087292a73b203df217831e3003f0dfccc676b
Oliver Schmitz <o.schmitz@uu.nl>
2025-02-28 20:50:03 +0100

    WIP Python moc

--- source/pcraster_moc/python/__init__.py
+++ source/pcraster_moc/python/__init__.py
@@ -8,9 +8,17 @@ import sys
 # Otherwise our dlls won't be found when our Python extensions are loaded
 # by Python.
 if sys.platform == "win32":
-    pcraster_app_path = shutil.which("pcrcalc.exe")
-    if pcraster_app_path:
-        pcraster_bin_pathname = pathlib.Path(pcraster_app_path).parent.absolute()
-        os.add_dll_directory(pcraster_bin_pathname)
+    _pcraster_app_path = shutil.which("pcrcalc.exe")
+    if _pcraster_app_path:
+        _pcraster_bin_pathname = pathlib.Path(_pcraster_app_path).parent
+        os.add_dll_directory(_pcraster_bin_pathname)
+        del _pcraster_bin_pathname
+    del _pcraster_app_path
 
-from ._pcraster_moc import *
+
+from ._pcraster_moc import initialise
+
+__all__ = ["initialise"]
+
+# Remove symbols imported for internal use
+del os, pathlib, shutil, sys

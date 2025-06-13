06df2247fea1de4b96ef1350d91ff1b966258c22
Oliver Schmitz <o.schmitz@uu.nl>
2025-06-13 13:19:17 +0200

    WIP CI

--- environment/conda/build.sh
+++ environment/conda/build.sh
@@ -20,11 +20,13 @@ CMAKE_ARGS="${CMAKE_ARGS} -DPython_NumPy_INCLUDE_DIR:PATH=${Python_NumPy_INCLUDE
 # NOTE
 # cmake -E env CFLAGS="-Werror=deprecated-declarations" CXXFLAGS="-Werror=deprecated-declarations" \
 cmake -E env CFLAGS="${CFLAGS} -Werror=deprecated-declarations" CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY -Werror=deprecated-declarations" \
-cmake ${CMAKE_ARGS} -S $SRC_DIR -B build \
-  -G"Ninja" -DCMAKE_BUILD_TYPE=Release \
+cmake ${CMAKE_ARGS} \
+  -S $SRC_DIR \
+  -B build \
+  -G"Ninja" \
+  -D CMAKE_BUILD_TYPE=Release \
   -D CMAKE_INSTALL_PREFIX="${PREFIX}" \
   -D PCRASTER_PYTHON_INSTALL_DIR=${SP_DIR} \
-  -D PCRASTER_WITH_FLAGS_NATIVE=OFF \
   -D PCRASTER_BUILD_TEST=ON
 
 cmake --build build --target all --parallel ${CPU_COUNT}

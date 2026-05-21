2447bcf9ea7b9934df2ee74adbc67e27dd742343
Oliver Schmitz <o.schmitz@uu.nl>
2026-05-21 18:21:10 +0200

    WIP CI

--- environment/configuration/receipe/build.sh
+++ environment/configuration/receipe/build.sh
@@ -20,8 +20,18 @@ cd $SRC_DIR
 mkdir -p build && cd build
 
 
+
+# In case find_package Python fails: force it
+PYTHON=python
+Python_INCLUDE_DIR="$(${PYTHON} -c 'import sysconfig; print(sysconfig.get_path("include"))')"
+Python_NumPy_INCLUDE_DIR="$(${PYTHON} -c 'import numpy;print(numpy.get_include())')"
+CMAKE_ARGS="${CMAKE_ARGS} -DPython3_EXECUTABLE:PATH=${PYTHON}"
+CMAKE_ARGS="${CMAKE_ARGS} -DPython3_INCLUDE_DIR:PATH=${Python_INCLUDE_DIR}"
+CMAKE_ARGS="${CMAKE_ARGS} -DPython3_NumPy_INCLUDE_DIR:PATH=${Python_NumPy_INCLUDE_DIR}"
+
+
 # master branch
-cmake .. -G"Ninja" -DCMAKE_BUILD_TYPE=Release \
+cmake ${CMAKE_ARGS} .. -G"Ninja" -DCMAKE_BUILD_TYPE=Release \
   -D CMAKE_INSTALL_PREFIX="${PREFIX}" \
   -D PCRASTER_PYTHON_INSTALL_DIR=${SP_DIR} \
   -D PCRASTER_WITH_FLAGS_NATIVE=OFF \
@@ -32,6 +42,6 @@ cmake --build . --target all
 
 export LOGNAME=pcrtester
 
-# ctest --output-on-failure
+ctest --output-on-failure
 
 cmake --build . --target install
